7018da5bafd481115e9b81b9574bd4c02457467c
qasid <qasid333444@gmail.com>
2024-05-07 15:49:03 +0400

    add employess and customer entities

---
+++ BANK/domain/Entites/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace BANK.domain.Entites
+{
+    public class Employee
+    {
+        int ID { get; set; }
+        string name { get; set; }
+        string surname { get; set; }
+        int positionId { get; set; }
+        string email { get; set; }
+        string phone { get; set; }
+        string address { get; set; }
+        DateTime birthDate { get; set; }
+        DateTime startWork {  get; set; }
+
+    }
+}
