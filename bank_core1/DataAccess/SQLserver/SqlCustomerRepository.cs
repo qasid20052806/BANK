f9f26dcf9454d5830332f9619b23bc433f23d523
qasid <qasid333444@gmail.com>
2024-05-29 16:56:17 +0400

    slm

--- bank_core1/DataAccess/SQLserver/SqlCustomerRepository.cs
+++ bank_core1/DataAccess/SQLserver/SqlCustomerRepository.cs
@@ -6,31 +6,34 @@ using System.Data.SqlClient;
 
 namespace bank_core1.DataAccess.SQLserver
 {
+
     public class SqlCustomerRepository : ICustomerRepository
     {
         private readonly string _connectionString = @" Data Source=QASID\MSSQLSERVER02;
                                             Initial Catalog=BANK;
                                              Integrated Security=true";
+
+
+
         SqlCustomerRepository(string connectionString)
         {
             _connectionString = connectionString;
         }
 
-        public Customer Add(Customer item)
+        public int Add(Customer item)
         {
             if (item == null)
             {
                 throw new ArgumentNullException(nameof(item), "Customer cannot be null");
             }
 
-
-            string query = @"INSERT INTO Customers (ID, FirstName, LastName, BirthDate, JoinTime, Address, PhoneNumber, Email) 
-                         VALUES (@Id, @Name, @LastName, @BirthDate, @JoinTime, @Address, @PhoneNumber, @Email)";
-
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
+                connection.Open();
+                string query = @"INSERT INTO Customers (ID, FirstName, LastName, BirthDate, JoinTime, Address, PhoneNumber, Email) 
+                         VALUES (@Id, @Name, @LastName, @BirthDate, @JoinTime, @Address, @PhoneNumber, @Email)";
                 SqlCommand command = new SqlCommand(query, connection);
-               
+
                 command.Parameters.AddWithValue("@Id", item.Id);
                 command.Parameters.AddWithValue("@Name", item.FirstName);
                 command.Parameters.AddWithValue("@LastName", item.LastName);
@@ -40,18 +43,7 @@ namespace bank_core1.DataAccess.SQLserver
                 command.Parameters.AddWithValue("@PhoneNumber", item.PhoneNumber);
                 command.Parameters.AddWithValue("@Email", item.Email);
 
-                try
-                {
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    Console.WriteLine($"Customer added: {item}");
-                    
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"An error occurred: {ex.Message}");
-                }
-                return (Customer)command.ExecuteScalar();
+                return (int)command.ExecuteScalar();
 
             }
         }
